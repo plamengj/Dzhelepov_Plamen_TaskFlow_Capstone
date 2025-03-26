f9fe140989177bc1e692ed7116935a8082cbaa78
plamengj <plamen.gj@gmail.com>
2025-03-26 10:29:40 -0400

    reverting back

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -43,8 +43,8 @@ const Login = () => {
                     auto_select: false,
                     cancel_on_tap_outside: true,
                     context: 'signin',
-                    ux_mode: 'redirect',
-                    redirect_uri: window.location.origin
+                    ux_mode: 'popup',
+                    popup_parent_id: 'googleSignInDiv'
                 });
                 window.google.accounts.id.renderButton(
                     document.getElementById('googleSignInDiv'),
